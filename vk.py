d224bbf7952f9c719165f8c9136ed46e20c6d8b0
va1holl <pezzatini@proton.me>
2025-02-08 18:33:00 +0200

    Добавил пару кнопок

--- vk.py
+++ vk.py
@@ -1,12 +1,16 @@
 import selenium.webdriver.common.by
 import seleniumwire.webdriver
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import get_user
 
-proxy_username = "modeler_rOgCya"
-proxy_password = "idVxnSDxf1Kv"
+
+proxy_username = "modeler_NQo1GM"
+proxy_password = "tiABEsHseXyV"
 proxy_address = "86.104.75.67"
-proxy_port = "13523"
+proxy_port = "13522"
 
 
 seleniumwire_options = {
@@ -20,20 +24,33 @@ seleniumwire_options = {
 options = seleniumwire.webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
-driver = seleniumwire.webdriver.Chrome(options = options, seleniumwire_options = seleniumwire_options) 
+driver = seleniumwire.webdriver.Chrome(options=options, seleniumwire_options=seleniumwire_options)
 driver.get("https://vk.com/")
 
-element = driver.find_element(selenium.webdriver.common.by.By.XPATH, '/html/body/div[14]/div/div/div/div[3]/div/div/div[2]/div[1]/div/div/section/div/div/div/div/div/div[2]/div/button[1]/span')
-element.click()
+wait = WebDriverWait(driver, 10)
 
-element = driver.find_element(selenium.webdriver.common.by.By.XPATH, '/html/body/div[14]/div/div/div/div[3]/div/div/div[2]/div[1]/div/div/section/div/div/div/div/div/form/div[1]/div[1]/div/label[2]')
-element.click()
+button = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[2]/div/button[1]/span')))
+button.click()
+
+label = wait.until(EC.element_to_be_clickable((By.XPATH, '//form/div[1]/div[1]/div/label[2]')))
+label.click()
 
 user = get_user.get_user(0)
 print(user[0])
 
-element = driver.find_element(selenium.webdriver.common.by.By.XPATH, '/html/body/div[14]/div/div/div/div[3]/div/div/div[2]/div[1]/div/div/section/div/div/div/div/div/form/div[1]/span/div/div/input')
-element.send_keys(user[0])
+input_login = wait.until(EC.presence_of_element_located((By.XPATH, '//form/div[1]/span/div/div/input')))
+input_login.send_keys(user[0])
 
-element = driver.find_element(selenium.webdriver.common.by.By.XPATH, '/html/body/div[14]/div/div/div/div[3]/div/div/div[2]/div[1]/div/div/section/div/div/div/div/div/form/button[1]/span')
-element.click()
+button_login = wait.until(EC.element_to_be_clickable((By.XPATH, '//form/button[1]/span')))
+button_login.click()
+
+
+val = '//*[@id="root"]/div/div/div/div/div[1]/div/div/div/div/div/form/div[1]/div[3]/div/div/input'
+
+input_password = wait.until(EC.presence_of_element_located((By.XPATH, val)))
+input_password.send_keys(user[1])
+
+button_pass = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div/div[1]/div/div/div/div/div/form/div[2]/button/span")))
+button_pass.click()
+
+int = input()
